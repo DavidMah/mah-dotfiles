ad93522349c4ca0958b65c974fea82973e59fdae
David Mah <MahHaha@gmail.com>
2013-06-11 16:29:23 -0700

    Changed link to overwrite and backup

--- link.sh
+++ link.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 # This must be run within the config directory
 CONFIG_DIR=$(pwd)
-ln -s $CONFIG_DIR/vim ~/.vim
-ln -s $CONFIG_DIR/vimrc ~/.vimrc
-ln -s $CONFIG_DIR/gvimrc ~/.gvimrc
-ln -s $CONFIG_DIR/irbrc ~/.irbrc
-ln -s $CONFIG_DIR/terminator ~/.terminator
-ln -s ~/.terminator ~/.config/terminator/config
-ln -s $CONFIG_DIR/bashrc ~/.bashrc
-ln -s $CONFIG_DIR/zshrc ~/.zshrc
-ln -s $CONFIG_DIR/oh-my-zsh ~/.oh-my-zsh
-ln -s $CONFIG_DIR/vmailrc ~/.vmailrc
+ln -b -s $CONFIG_DIR/vim ~/.vim
+ln -b -s $CONFIG_DIR/vimrc ~/.vimrc
+ln -b -s $CONFIG_DIR/gvimrc ~/.gvimrc
+ln -b -s $CONFIG_DIR/irbrc ~/.irbrc
+ln -b -s $CONFIG_DIR/terminator ~/.terminator
+ln -b -s ~/.terminator ~/.config/terminator/config
+ln -b -s $CONFIG_DIR/bashrc ~/.bashrc
+ln -b -s $CONFIG_DIR/zshrc ~/.zshrc
+ln -b -s $CONFIG_DIR/oh-my-zsh ~/.oh-my-zsh
+ln -b -s $CONFIG_DIR/vmailrc ~/.vmailrc
